d14452137ed44a1bcbd25f60341fd72c71be2710
Kamily <kamily7803@gmail.com>
2025-08-13 14:14:01 -0300

    feat: create categories table with user relationship

---
+++ database/migrations/2025_08_13_160716_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('color', 7)->nullable();
+            $table->string('emoji', 191)->nullable();
+            $table->enum('type', ['income', 'expense']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
