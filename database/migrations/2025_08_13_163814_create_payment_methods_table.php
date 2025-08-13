e935e568d83bc70f2f334b078f4db67a6df8f8d5
Kamily <kamily7803@gmail.com>
2025-08-13 14:15:42 -0300

    feat: create payment methods table with user relationship

---
+++ database/migrations/2025_08_13_163814_create_payment_methods_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->enum('type', ['credit_card', 'debit_card', 'cash', 'pix']);
+            $table->decimal('limit', 15, 2)->nullable();
+            $table->unsignedTinyInteger('closing_day')->nullable();
+            $table->unsignedTinyInteger('due_day')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+};
