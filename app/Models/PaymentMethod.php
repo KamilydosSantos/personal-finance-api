dccd711d13f2ea0b33d9702ecba7de3bd59dd09d
Kamily <kamily7803@gmail.com>
2025-08-13 15:47:43 -0300

    feat: create PaymentMethod model with relationships to User and Transactions

---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PaymentMethod extends Model
+{
+    use HasFactory;
+
+    protected $table = 'payment_methods';
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'type',
+        'limit',
+        'closing_day',
+        'due_day',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
