e6b6398d3a2541bbc4e92b57b0fff678ff31c7a8
Kamily <kamily7803@gmail.com>
2025-08-14 16:59:47 -0300

    feat: create UserService with createUser method

---
+++ app/Services/UserService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\UserRepositoryInterface;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    protected UserRepositoryInterface $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function createUser(array $data)
+    {
+        $data['password'] = Hash::make($data['password']);
+        return $this->userRepository->create($data);
+    }
+}
