b32cf9660ab1bfeb32aeac2c1954bbf9289a09d0
Kamily <kamily7803@gmail.com>
2025-08-14 19:11:15 -0300

    feat: add global API AuthenticationException handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,13 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (AuthenticationException $e, Request $request): ?JsonResponse {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Token inválido ou não fornecido.'
+                ], 401);
+            }
+
+            return null;
+        });
     })->create();
