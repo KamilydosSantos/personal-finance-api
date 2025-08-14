2def4dcde2d9c2088bd77e8a9d84456b28d81fa7
Kamily <kamily7803@gmail.com>
2025-08-14 17:07:14 -0300

    feat: add getUserById method in UserService

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -19,4 +19,9 @@ class UserService
         $data['password'] = Hash::make($data['password']);
         return $this->userRepository->create($data);
     }
+
+    public function getUserById(int $id)
+    {
+        return $this->userRepository->findById($id);
+    }
 }
