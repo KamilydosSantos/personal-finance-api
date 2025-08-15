9e5973e881e86f3334489c38213c30ab3be224c5
Kamily <kamily7803@gmail.com>
2025-08-15 10:29:13 -0300

    feat: add logout method to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use App\Services\AuthService;
 use Illuminate\Http\JsonResponse;
@@ -25,5 +26,13 @@ class AuthController extends Controller
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 401);
         }
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $user = $request->user();
+        $this->authService->logout($user);
+
+        return response()->json(['message' => 'Logout realizado com sucesso']);
     }
 }
