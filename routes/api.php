b9452e079764b7562d397c23ff32d869a885a3fd
Kamily <kamily7803@gmail.com>
2025-08-15 10:32:30 -0300

    feat: add protected logout route using Sanctum auth middleware

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
 Route::post('/users', [UserController::class, 'store']);
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
